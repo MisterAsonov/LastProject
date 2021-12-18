cec782681d9dd972df3d6d11f55b6132c9604716
valery <valerki357@gmail.com>
2021-12-19 00:36:09 +0200

    Another Project

--- app/src/main/java/com/example/lastproject/FirstFragment.java
+++ app/src/main/java/com/example/lastproject/FirstFragment.java
@@ -3,10 +3,14 @@ package com.example.lastproject;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -53,7 +57,12 @@ public class FirstFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
