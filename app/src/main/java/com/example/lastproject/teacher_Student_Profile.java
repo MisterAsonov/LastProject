ada9324ab37de9d0881f2f5e647fd6499fa57154
valery <valerki357@gmail.com>
2022-03-13 21:20:54 +0200

    FireBase upload photo

--- app/src/main/java/com/example/lastproject/teacher_Student_Profile.java
+++ app/src/main/java/com/example/lastproject/teacher_Student_Profile.java
@@ -9,42 +9,59 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class teacher_Student_Profile extends AppCompatActivity {
-    EditText ETname, ETlastname;
-    TextView ETemail, ETwho;
+
+    TextView ETname, ETemail, ETwho;
+    ImageView photo;
     String name, lastname, email, who, id;
     String key;
     String url_photo;
     DatabaseReference mPostReference;
+    DatabaseReference user_ref;
 
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.studet_details_menu, menu);
+        inflater.inflate(R.menu.teacher_student_profile, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        Toast.makeText(teacher_Student_Profile.this, "Enter", Toast.LENGTH_SHORT).show();
-        if(item.getItemId() == R.id.delete){
 
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            String userID = user.getUid();
-            mPostReference = FirebaseDatabase.getInstance().getReference().child("Groups").child(userID).child(key);
-            mPostReference.removeValue();
-            return true;
+        switch (item.getItemId()) {
+            case R.id.delite_student:
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String userID = user.getUid();
+                mPostReference = FirebaseDatabase.getInstance().getReference().child("Groups").child(userID).child(key);
+                mPostReference.removeValue();
+                return true;
 
+            case R.id.change_student:
+                /**
+                 * FirebaseAuth.getInstance().signOut();
+                 *                 Intent intent = new Intent(teacher_Student_Profile.this, LoginScreen.class);
+                 *                 startActivity(intent);
+                 */
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -52,11 +69,15 @@ public class teacher_Student_Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_student_profile);
 
-        setSupportActionBar(findViewById(R.id.toolbar2));
-        ETname = findViewById(R.id.et_name_of_student);
-        ETlastname = findViewById(R.id.et_lastname_of_student);
-        ETemail = findViewById(R.id.et_email_of_student);
-        ETwho = findViewById(R.id.et_who_of_student);
+         ETname = findViewById(R.id.name_of_student_profile);
+         ETemail = findViewById(R.id.email);
+         ETwho = findViewById(R.id.who20);
+         photo = findViewById(R.id.photo_of_student_profile);
+
+        setSupportActionBar(findViewById(R.id.toolbar6));
+
+        user_ref = FirebaseDatabase.getInstance().
+                getReference("Users");
 
         Intent intent = getIntent();
 
@@ -68,9 +89,13 @@ public class teacher_Student_Profile extends AppCompatActivity {
         key = intent.getStringExtra("key");
         url_photo = intent.getStringExtra("url_photo");
 
-        ETname.setText(name);
-        ETlastname.setText(lastname);
+        ETname.setText(name + " " + lastname);
         ETemail.setText(email);
         ETwho.setText(who);
+
+        if(!url_photo.equals(""))
+            Picasso.get().load(url_photo).into(photo);
+
+
     }
 }
